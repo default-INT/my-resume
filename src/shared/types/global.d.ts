4975a24a1777f8f1eae55851dadd897cc1896635
Yauheni Trafimau <yauheni.trafimau@itechart-group.com>
2025-09-11 13:45:28 +0300

    fix tsc errors and compress avatar image

--- src/shared/types/global.d.ts
+++ src/shared/types/global.d.ts
@@ -1,8 +1,6 @@
 declare module '*.svg' {
   import * as React from 'react';
 
-  const ReactComponent: React.VFC<React.SVGProps<SVGSVGElement>>;
-  const svg: string;
-  export { ReactComponent };
-  export default svg;
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
 }
